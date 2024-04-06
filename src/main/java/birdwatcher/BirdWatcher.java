f68d11616ddb842cc53b6c6a7b5117d5b19baa39
Robert <robert.sitek16@gmail.com>
2024-04-06 22:07:10 +0200

    Exercism - Java - Bird Watcher

---
+++ src/main/java/birdwatcher/BirdWatcher.java
@@ -0,0 +1,36 @@
+package birdwatcher;
+
+import java.util.Arrays;
+
+public class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return birdsPerDay;
+    }
+
+    public int getToday() {
+        return birdsPerDay[birdsPerDay.length - 1];
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length - 1] += 1;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        return Arrays.stream(birdsPerDay).anyMatch(x -> x == 0);
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        return Arrays.stream(birdsPerDay).limit(numberOfDays).sum();
+    }
+
+    public int getBusyDays() {
+        return Arrays.stream(birdsPerDay).filter(x -> x >= 5).toArray().length;
+    }
+
+}
